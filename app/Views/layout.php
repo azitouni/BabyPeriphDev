7ba0f6348210c2a118a42ffc692eddcc03a2a111
Gino78 <roberjot.gino@gmail.com>
2016-12-14 12:15:29 +0100

    vendor

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -3,13 +3,28 @@
 <head>
 	<meta charset="UTF-8">
 	<title><?= $this->e($title) ?></title>
-
+	<link rel="stylesheet" href="<?= $this->assetUrl('vendor/bootstrap/css/bootstrap.min.css') ?>">
+	<link rel="stylesheet" href="<?= $this->assetUrl('vendor/font-awesome/css/font-awesome.min.css') ?>">
 	<link rel="stylesheet" href="<?= $this->assetUrl('css/style.css') ?>">
+
+	
 </head>
 <body>
-	<div class="container">
+	
 		<header>
-			<h1>W :: <?= $this->e($title) ?></h1>
+			
+				<div class="row">
+					<div class="col-xs-12 col-md-3 ">
+						<img class="logo img-responsive" src="<?= $this->assetUrl('img/baby-periph-logo.png') ?>" alt="">
+					</div>
+					<div class="col-xs-12 col-md-3 ">
+						<img class="titre1 img-responsive" src="<?= $this->assetUrl('img/Baby-letters-logo.png') ?>" alt="">
+					</div>
+					<div class="col-xs-12 col-md-3 ">
+						<img class="titre2 img-responsive" src="<?= $this->assetUrl('img/Periph-letters-logo.png') ?>" alt="">
+					</div>
+				</div>
+			</div>
 		</header>
 
 		<section>
@@ -18,6 +33,6 @@
 
 		<footer>
 		</footer>
-	</div>
+	
 </body>
 </html>
